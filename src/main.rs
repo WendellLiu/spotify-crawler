05716df2217dcb1c791d62cd5ed86df8606959ea
Wendell Liu <cuk.bas@gmail.com>
2021-08-24 21:59:25 +0800

    feat: introduce clap

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,45 @@ mod dto;
 mod job;
 mod storage;
 
+use clap::{AppSettings, Clap};
 use config::{SystemConfig, SYSTEM_CONFIG};
 use job::crawl_new_release;
+
+#[derive(Clap, Debug)]
+#[clap(version = "1.0", author = "Wendell Liu <cuk.bas@gmail.com>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    /// file path of config
+    #[clap(short, long, default_value = "crawler.conf")]
+    config: String,
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap, Debug)]
+enum SubCommand {
+    #[clap(version = "1.0", author = "Wendell Liu <cuk.bas@gmail.com>")]
+    NewRelease,
+    #[clap(version = "1.0", author = "Wendell Liu <cuk.bas@gmail.com>")]
+    Recommendation,
+}
+
+#[derive(Clap)]
+struct NewRelease {}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let system_config_instance = SystemConfig::new();
     SYSTEM_CONFIG.set(system_config_instance).unwrap();
 
-    crawl_new_release::run().await?;
+    let opts: Opts = Opts::parse();
+
+    match opts.subcmd {
+        SubCommand::NewRelease => {
+            crawl_new_release::run().await?;
+        }
+        _ => {}
+    }
+
     Ok(())
 }
